4eed50d4a5729acc39f373b411c9660d22debc1f
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-30 14:05:25 +0200

    prototype

--- src/Components/Home/Search.jsx
+++ src/Components/Home/Search.jsx
@@ -34,6 +34,7 @@ const Search = () => {
 
   useEffect(() => {
     calculateTotalPages();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [meals]);
 
   const fetchMealCategories = async () => {
